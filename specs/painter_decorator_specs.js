0417d03716dce917d0846bed1c07a410ac12a21f
mike <>
2020-03-31 18:24:15 +0100

    decorator tests on specs passed 2

--- specs/painter_decorator_specs.js
+++ specs/painter_decorator_specs.js
@@ -1,2 +1,16 @@
 const assert = require('assert');
-const painter = require('../tdd_painter_decorator.js');
+const Decorator = require('../tdd_painter_decorator.js');
+
+describe('decorator', function () {
+  it('should have paint stock', function () {
+    const decorator = new Decorator(0);
+    const actual = decorator.paintStock;
+    assert.strictEqual(actual, 0);
+  });
+
+  it('should add to paint stock', function () {
+    const decorator = new Decorator(1);
+    const actual = decorator.paintStock;
+    assert.strictEqual(actual, 1);
+  });
+});
